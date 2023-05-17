38febb82beb1581a2a990c52c18f69f81c7ba2c4
Gregory Vincic <gregory.vincic@non.se.com>
2023-05-17 13:51:13 +0200

    Add ex/01

---
+++ ex/01/main.go
@@ -0,0 +1,12 @@
+// Command rebel generates rebelious statement.
+package main
+
+func main() {
+	Shout()
+}
+
+// Shout writes a statement to os.Stdout
+func Shout() {
+	fmt.Println("We are Rebelz!")
+}
+
